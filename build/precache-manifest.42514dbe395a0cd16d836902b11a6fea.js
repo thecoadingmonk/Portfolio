e63edef128adb56b5de31ad1f455ec33e2cafc85
thecoadingmonk <thecoadingmonk@gmail.com>
2020-12-08 01:35:01 +0530

    updated portfolio with warning

---
+++ build/precache-manifest.42514dbe395a0cd16d836902b11a6fea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc334e42c7449e776a1c50f297b5d776",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "15e8cc67bff41cf8d488",
+    "url": "/Portfolio/static/css/main.802a3ca8.chunk.css"
+  },
+  {
+    "revision": "2abb82f6e211cc3558b4",
+    "url": "/Portfolio/static/js/2.dffbf2e9.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/Portfolio/static/js/2.dffbf2e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e8cc67bff41cf8d488",
+    "url": "/Portfolio/static/js/main.bf54dfa8.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "6a6f83a305840e005b0943bda76dc0a4",
+    "url": "/Portfolio/static/media/grid.6a6f83a3.svg"
+  },
+  {
+    "revision": "4e116130cd87d83272076ff9747b9721",
+    "url": "/Portfolio/static/media/profile_pic.4e116130.jpg"
+  }
+]);
